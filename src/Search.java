51ac89d6c11855a8782f8b71239f8cde72b682ff
al4851 <al4851@rit.edu>
2022-02-16 01:39:15 -0500

    BFS DFS AS complete

--- src/Search.java
+++ src/Search.java
@@ -15,7 +15,7 @@ public class Search {
         } else {
             HashMap<String, CityInfo> cityInfo = readCityInfo();
             String[] input = readInput(cityInfo, args[0]);
-            output(cityInfo, input, args[1]);
+            result(cityInfo, args[1], input);
         }
     }
 
@@ -56,7 +56,7 @@ public class Search {
     }
 
     /**
-     * Read input from user or from file
+     * Read start and destination city from user or from file
      *
      * @param cityInfo information of cities
      * @param fileName file name of input file
@@ -88,7 +88,6 @@ public class Search {
                 System.exit(0);
             }
         }
-
         return input;
     }
 
@@ -108,38 +107,64 @@ public class Search {
         }
     }
 
-    public static void output(HashMap<String, CityInfo> cityInfo, String[] input, String fileName) {
-        LinkedList<String> path = BFS(cityInfo, input);
-        String previous = input[0];
-        double distance = 0;
+    public static void result(HashMap<String, CityInfo> cityInfo,
+                              String fileName, String[] input) {
+        LinkedList<String> path;
         if (fileName.equalsIgnoreCase("-")) {
+            path = BFS(cityInfo, input);
             System.out.println("\nBreadth-First Search Results:");
-            System.out.println(input[0]);
-            for (int i = 1; i < path.size(); i++) {
-                System.out.println(path.get(i));
-                distance += distance(cityInfo.get(path.get(i)), cityInfo.get(previous));
-                previous = path.get(i);
-            }
-            System.out.printf("That took %d hops to find.\n", path.size() - 1);
-            System.out.printf("Total distance = %d miles.\n", Math.round(distance));
+            stdOutput(cityInfo, path);
+            path = DFS(cityInfo, input);
+            System.out.println("\nDepth-First Search Results:");
+            stdOutput(cityInfo, path);
+            path = AS(cityInfo, input);
+            System.out.println("\nA* Search Results:");
+            stdOutput(cityInfo, path);
         } else {
             try {
                 FileWriter fileWriter = new FileWriter(fileName);
                 PrintWriter printWriter = new PrintWriter(fileWriter);
+                path = BFS(cityInfo, input);
                 printWriter.println("\nBreadth-First Search Results:");
-                printWriter.println(input[0]);
-                for (int i = 1; i < path.size(); i++) {
-                    printWriter.println(path.get(i));
-                    distance += distance(cityInfo.get(path.get(i)), cityInfo.get(previous));
-                    previous = path.get(i);
-                }
-                printWriter.printf("That took %d hops to find.\n", path.size() - 1);
-                printWriter.printf("Total distance = %d miles.\n", Math.round(distance));
+                outputFile(cityInfo, path, printWriter);
+                path = DFS(cityInfo, input);
+                printWriter.println("\nDepth-First Search Results:");
+                outputFile(cityInfo, path, printWriter);
+                //path = AS(cityInfo, input);
+                printWriter.println("\nA* Search Results:");
+                //outputFile(cityInfo, path, printWriter);
                 printWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    public static void stdOutput(HashMap<String, CityInfo> cityInfo, LinkedList<String> path) {
+        String previous = path.get(0);
+        double distance = 0;
+        System.out.println(path.get(0));
+        for (int i = 1; i < path.size(); i++) {
+            System.out.println(path.get(i));
+            distance += distance(cityInfo.get(path.get(i)), cityInfo.get(previous));
+            previous = path.get(i);
+        }
+        System.out.printf("That took %d hops to find.\n", path.size() - 1);
+        System.out.printf("Total distance = %d miles.\n", Math.round(distance));
+    }
+
+    public static void outputFile(HashMap<String, CityInfo> cityInfo,
+                                  LinkedList<String> path, PrintWriter printWriter) {
+        String previous = path.get(0);
+        double distance = 0;
+        printWriter.println(path.get(0));
+        for (int i = 1; i < path.size(); i++) {
+            printWriter.println(path.get(i));
+            distance += distance(cityInfo.get(path.get(i)), cityInfo.get(previous));
+            previous = path.get(i);
+        }
+        printWriter.printf("That took %d hops to find.\n", path.size() - 1);
+        printWriter.printf("Total distance = %d miles.\n\n", Math.round(distance));
     }
 
     public static LinkedList<String> BFS(HashMap<String, CityInfo> cityInfo, String[] input) {
@@ -156,8 +181,10 @@ public class Search {
             if (current.equalsIgnoreCase(end)) {
                 break;
             }
-            Collections.sort(cityInfo.get(current).edge);
-            for (String s : cityInfo.get(current).edge) {
+
+            LinkedList<String> edge = new LinkedList<>(cityInfo.get(current).edge);
+            Collections.sort(edge);
+            for (String s : edge) {
                 if (!predecessors.containsKey(s)) {
                     if (cityInfo.get(current).edge.contains(end)) {
                         predecessors.put(end, current);
@@ -182,18 +209,85 @@ public class Search {
         return path;
     }
 
-    public static String[] DFS(HashMap<String, CityInfo> cityInfo, String[] input) {
+    public static LinkedList<String> DFS(HashMap<String, CityInfo> cityInfo, String[] input) {
         String start = input[0];
         String end = input[1];
+        Stack<String> stack = new Stack<>();
+        LinkedList<String> path = new LinkedList<>();
+        HashMap<String, String> predecessors = new HashMap<>();
+        stack.push(start);
+        predecessors.put(start, null);
 
-        return null;
+        while (!stack.isEmpty()) {
+            String current = stack.pop();
+            if (current.equalsIgnoreCase(end)) {
+                break;
+            }
+
+            LinkedList<String> edge = new LinkedList<>(cityInfo.get(current).edge);
+            edge.sort(Collections.reverseOrder());
+            for (String s : edge) {
+                if (!predecessors.containsKey(s)) {
+                    if (cityInfo.get(current).edge.contains(end)) {
+                        predecessors.put(end, current);
+                        stack.push(end);
+                        break;
+                    }
+                    predecessors.put(s, current);
+                    stack.push(s);
+                }
+            }
+        }
+
+        if (predecessors.containsKey(end)) {
+            String current = end;
+            while (!current.equalsIgnoreCase(start)) {
+                path.add(0, current);
+                current = predecessors.get(current);
+            }
+            path.add(0, start);
+        }
+
+        return path;
     }
 
-    public static String[] AS(HashMap<String, CityInfo> cityInfo, String[] input) {
+    public static LinkedList<String> AS(HashMap<String, CityInfo> cityInfo, String[] input) {
         String start = input[0];
         String end = input[1];
+        LinkedList<ASCityInfo> queue = new LinkedList<>();
+        LinkedList<String> path = new LinkedList<>();
+        HashMap<ASCityInfo, ASCityInfo> predecessors = new HashMap<>();
+        double gn, hn;
+        ASCityInfo goal = null;
+        double distance = distance(cityInfo.get(start), cityInfo.get(end));
+        queue.add(new ASCityInfo(start, null, 0, distance, distance));
 
-        return null;
+        while (!queue.isEmpty()) {
+            ASCityInfo current = queue.remove();
+            if (current.name.equalsIgnoreCase(end)) {
+                goal = current;
+                System.out.println(goal.name);
+                break;
+            }
+            for (String s : cityInfo.get(current.name).edge) {
+                gn = distance(cityInfo.get(s), cityInfo.get(current.name)) + current.gn;
+                hn = distance(cityInfo.get(s), cityInfo.get(end));
+                ASCityInfo next = new ASCityInfo(s, current, gn, hn, gn + hn);
+                predecessors.put(next, current);
+                queue.add(next);
+            }
+            Collections.sort(queue);
+        }
+
+        assert goal != null;
+        path.add(0, goal.name);
+        ASCityInfo current = goal;
+        while (!current.name.equalsIgnoreCase(start)) {
+            path.add(0, current.previous.name);
+            current = predecessors.get(current);
+        }
+
+        return path;
     }
 
     public static double distance(CityInfo city1, CityInfo city2) {
@@ -215,4 +309,32 @@ public class Search {
             this.lon = lon;
         }
     }
+
+    static class ASCityInfo implements Comparable<ASCityInfo>{
+        String name;
+        ASCityInfo previous;
+        double gn;
+        double hn;
+        double fn;
+
+        public ASCityInfo(String name, ASCityInfo previous, double gn, double hn, double fn) {
+            this.name = name;
+            this.previous = previous;
+            this.gn = gn;
+            this.hn = hn;
+            this.fn = fn;
+        }
+
+
+        @Override
+        public int compareTo(ASCityInfo o) {
+            if (this.fn < o.fn)
+                return -1;
+            else if (o.fn < this.fn)
+                return 1;
+            return 0;
+        }
+    }
+
+
 }
